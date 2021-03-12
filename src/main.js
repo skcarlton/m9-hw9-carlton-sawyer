acc39d9d6b975cb5ec4b453fc9012e7335a41f28
Sawyer Carlton <skcarlton@gmail.com>
2021-03-12 17:10:05 -0500

    Add Dessert and IceCream classes

---
+++ src/main.js
@@ -0,0 +1,18 @@
+class Desserts {
+    constructor(type, calories) {
+        this.type = type;
+        this.calories = calories;
+    }
+}
+
+class IceCream extends Desserts {
+    constructor(type, calories, flavor, scoops) {
+        super(type, calories)
+        this.flavor = flavor;
+        this.scoops = scoops;
+    }
+    
+    includeSpoon() {
+        console.log('Here is your spoon!');
+    }
+}
